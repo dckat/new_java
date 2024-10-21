4618187b7ce3999a6b2b14d15612b9203e2ebd35
Kim Daecheol <kdc9619@naver.com>
2024-10-21 12:20:42 +0900

    feat: 15일차 참조형 변수 캐스팅 업로드(2)

---
+++ start1/src/main/java/org/example/day15/referencecast/Truck.java
@@ -0,0 +1,21 @@
+package org.example.day15.referencecast;
+
+public class Truck extends Car {
+    // 묵시적
+    // 필드: 2개
+    // 메소드: 2개
+
+    // 트럭 클래스에 필드 + 메소드 추가
+    int weight;
+    public void move() {
+        System.out.println(speed + "속도로 짐을 나르다.");
+    }
+
+    public String toString() {
+        return "Truck{" +
+                "weight=" + weight +
+                ", color='" + color + '\'' +
+                ", speed=" + speed +
+                '}';
+    }
+}
