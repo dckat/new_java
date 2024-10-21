e520ae03d5041ebd3894b6a61da3117b9e5e9e4d
Kim Daecheol <kdc9619@naver.com>
2024-10-21 12:20:19 +0900

    feat: 15일차 기본형 변수 캐스팅 업로드

---
+++ start1/src/main/java/org/example/day15/basiccast/Basic2.java
@@ -0,0 +1,14 @@
+package org.example.day15.basiccast;
+
+import javax.swing.*;
+
+public class Basic2 {
+    public static void main(String[] args) {
+        long x = 50L;
+        JFrame f = new JFrame();
+        f.setSize((int)x, 500);
+
+        // 기본형 형변환 중 강제형변환은
+        // 강제로 변환될 데이터 타입 범위 안에 있어야 가능
+    }
+}
