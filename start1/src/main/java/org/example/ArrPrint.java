3962644e546f5ca1bb677ec912a7f79cabbf055e
Kim Daecheol <kdc9619@naver.com>
2024-10-29 17:24:04 +0900

    feat: 배열 출력 템플릿 코드 구현

---
+++ start1/src/main/java/org/example/ArrPrint.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ArrPrint {
+
+    // 배열만 출력하는 기능 --> 메소드 정의
+    public static void print(int[] x) {
+        System.out.println(Arrays.toString(x));
+    }
+
+    public static void print(String[] x) {
+        System.out.println(Arrays.toString(x));
+    }
+
+    public static void print(double[] x) {
+        System.out.println(Arrays.toString(x));
+    }
+}
