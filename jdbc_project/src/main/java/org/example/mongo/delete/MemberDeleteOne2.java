a23c28bd55a7f4cddc3de0d4d799df7b637103ad
Kim Daecheol <kdc9619@naver.com>
2024-11-20 12:20:11 +0900

    feat: mongodb delete

---
+++ jdbc_project/src/main/java/org/example/mongo/delete/MemberDeleteOne2.java
@@ -0,0 +1,34 @@
+package org.example.mongo.delete;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MemberDeleteOne2 {
+    public static void main(String[] args) {
+        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+        mongoLogger.setLevel(Level.WARNING);
+
+        // 1. MongoClient 생성
+        MongoClient client = new MongoClient("localhost", 27017);
+
+        // 2. db연결, collection 연결
+        MongoDatabase db = client.getDatabase("shop");
+        MongoCollection<Document> member = db.getCollection("member");
+        System.out.println("member 컬렉션 연결 성공!!");
+
+        // 3. 전송할 js 생성
+        Document filter = new Document();
+        filter.append("id", "apple20");
+        filter.append("pw", "1234");
+
+        // 4. 전송, 결과처리
+        member.deleteOne(filter);   // db.member.deleteOne({id: "apple20", pw: "1234"})
+        System.out.println("mongoDB에 전송함.");
+        client.close();
+    }
+}
